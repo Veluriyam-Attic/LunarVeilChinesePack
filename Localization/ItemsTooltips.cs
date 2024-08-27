017892559a8d17b4f82003a4675576cfcde9bb7c
Nan_Ting <52440522+3380991070@users.noreply.github.com>
2024-08-27 12:19:40 +0800

    Create ItemsTooltips.cs

---
+++ Localization/ItemsTooltips.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Text;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace LunarVeilChinesePack.Localization
+{
+    public class ItemsTooltips : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            foreach (TooltipLine tooltip in tooltips)
+            {
+                //用一个字符串替换一个字符串
+                StringBuilder sb = new StringBuilder(tooltip.Text);
+                sb.Replace("Simple Brooch!", "胸针!");
+
+
+                tooltip.Text = sb.ToString();
+                //tooltip.Text = Regex.Replace(tooltip.Text,"?" + tooltip.Text +"?",sb.ToString());
+                
+                
+            }
+            base.ModifyTooltips(item, tooltips);
+        }
+    }
+}
