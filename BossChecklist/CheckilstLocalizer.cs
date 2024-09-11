985c61760104f37069df630f2ac9e4e2a42c0185
APlumTree <2310803020@qq.com>
2024-09-11 16:21:11 +0800

    Update CheckilstLocalizer.cs

--- BossChecklist/CheckilstLocalizer.cs
+++ BossChecklist/CheckilstLocalizer.cs
@@ -28,19 +28,19 @@ namespace LunarVeilChinesePack.BossChecklist
 
             #region DoDaedusIntegration(世遗者代达斯)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoDaedusIntegration", new() {
-                { "Disturb with the ruined tablet deep within the morrow.","" },
+                { "Disturb with the ruined tablet deep within the morrow.","扰动幽曦地下深处的遗迹石碑" },
             });
             #endregion
 
             #region DoDreadmireIntegration(绛渊恶巫)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoDreadmireIntegration", new() {
-                { "Destroy 3 dread monoliths to create a Dread medallion and summon her during night.","摧毁三个恐怖巨石来合成恐惧徽章以召唤." },
+                { "Destroy 3 dread monoliths to create a Dread medallion and summon her during night.","摧毁三个恐怖巨石来合成恐惧徽章在夜晚召唤." },
             });
             #endregion
 
             #region DoCommanderGintziaIntegration(金戈亚指挥官)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoCommanderGintziaIntegration", new() {
-                { "Randomly each day, if you have 3 npcs and have killed the Stone Guardian, an army will raid you!","随机的一天，当你击杀了石心圣卫并且拥有至少三个NPC时，一只军队将会袭击你！" },
+                { "Randomly each day, if you have 3 npcs and have killed the Stone Guardian, an army will raid you!","当你击杀了石心圣卫并且拥有至少三个NPC时，一支军队将将会入侵！" },
             });
             #endregion
 
@@ -61,7 +61,7 @@ namespace LunarVeilChinesePack.BossChecklist
             #region DoVerliaIntegration(皎月薇莉娅)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoVerliaIntegration", new() {
                 //Use a [i:{summonItem7}] at a shrine in the Cathedral over the ice biome!
-                { "Use a [i:","在雪原的一个教堂中，找到一个神龛并使用 [i:" },
+                { "Use a [i:","在雪原的仪殿中，找到圣坛并使用 [i:" },
                 {"] at a shrine in the Cathedral over the ice biome!","] 以召唤." }
             });
             #endregion
@@ -111,19 +111,19 @@ namespace LunarVeilChinesePack.BossChecklist
 
             #region DoBlazingSerpentIntegration(烈焰长蛇)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoBlazingSerpentIntegration", new() {
-                {"Use a Cursed Shard at an altar in the Fire Catacombs, it may appear...","在狱之墓穴中的祭坛使用诅咒残片，它或许会出现..." }
+                {"Use a Cursed Shard at an altar in the Fire Catacombs, it may appear...","在烈火墓穴中的祭坛使用诅咒残片，它或许会出现..." }
             });
             #endregion
 
             #region DoCogworkIntegration(废旧齿轮)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoCogworkIntegration", new() {
-                {"Use a Cursed Shard at an altar in the Trapped or Water Catacombs, it may appear...","在水之墓穴或陷阱墓穴中的祭坛使用诅咒残片，它或许会出现..." }
+                {"Use a Cursed Shard at an altar in the Trapped or Water Catacombs, it may appear...","在浸水墓穴或陷阱墓穴中的祭坛使用诅咒残片，它或许会出现..." }
             });
             #endregion
 
             #region DoWaterJellyfishIntegration(氵母)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoWaterJellyfishIntegration", new() {
-                {"Use a Cursed Shard at an altar in the Water Catacombs, it may appear...","在水之墓穴中的祭坛使用诅咒残片 它或许会出现..." }
+                {"Use a Cursed Shard at an altar in the Water Catacombs, it may appear...","在浸水墓穴中的祭坛使用诅咒残片 它或许会出现..." }
             });
             #endregion
 
@@ -135,13 +135,13 @@ namespace LunarVeilChinesePack.BossChecklist
 
             #region DoPandorasFireBoxIntegration(潘多拉火焰盒)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoPandorasFireBoxIntegration", new() {
-                {"Use a Cursed Shard at an altar in the Fire Catacombs, it may appear...","在狱之墓穴中的祭坛使用诅咒残片，它或许会出现..." }
+                {"Use a Cursed Shard at an altar in the Fire Catacombs, it may appear...","在烈火墓穴中的祭坛使用诅咒残片，它或许会出现..." }
             });
             #endregion
 
             #region DoNiiviIntegration(尼伊维)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoNiiviIntegration", new() {
-                {"Deal enough damage to Niivi to anger her, you may find her flying around The Great Illuria! -or sleeping...","对尼伊维造成足够的伤害后来激怒它.你可能会发现他在星月巨树附近飞行-或者在睡觉." }
+                {"Deal enough damage to Niivi to anger her, you may find her flying around The Great Illuria! -or sleeping...","对尼伊维造成足够的伤害后来激怒她.她一般会在星月巨树附近闲游-或者在睡觉." }
             });
             #endregion
 
@@ -153,7 +153,7 @@ namespace LunarVeilChinesePack.BossChecklist
 
             #region DoSupernovaFragmentIntegration(破碎超新星)
             ForceLocalizeSystem.LocalizeByTypeName("ModIntegrationsSystem", "DoSupernovaFragmentIntegration", new() {
-                {"Go down to the Cinderspark and nag Ordin to get a gift for Ereshkigal and she may give you something in return...","前往烬火之地，向奥丁讨要一份给eresh的礼物，接受礼物后，她可能会以你想要的东西来报答你." }
+                {"Go down to the Cinderspark and nag Ordin to get a gift for Ereshkigal and she may give you something in return...","前往烬火之地，向奥丁巴结一份给厄莉什基迦勒的礼物，接受礼物后，她可能会以你想要的东西来报答你." }
             });
             #endregion
         }
