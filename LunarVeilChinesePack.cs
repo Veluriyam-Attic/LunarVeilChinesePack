217ad3b338049cf2ab15cbdf34f0941de8105730
Henceforth <73875008+zlzhaidou@users.noreply.github.com>
2024-11-18 18:42:57 +0800

    1.4.1.7

--- LunarVeilChinesePack.cs
+++ LunarVeilChinesePack.cs
@@ -13,4 +13,14 @@ public class LunarVeilChinesePack : Mod
         old = old.Replace(old,newst);
         return old;
     }
+    public class LunarVeilCN : ModSystem
+    {
+        public override void PostSetupContent()
+        {
+            if (
+                    ModLoader.HasMod("LunarVeilCN")
+               )
+                Utils.ShowFancyErrorMessage("[c/ff0000:请关闭LunarVeil（穹月天纱mod）个人汉化]\n[c/ff0000:此汉化不全且长时间不更新，不适配新版本穹月天纱]\n[c/ff0000:以及同时使用多个汉化会造成命名混乱]", 1);
+        }
+    }
 }
